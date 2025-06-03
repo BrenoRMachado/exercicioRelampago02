234d5d94b5435dd0f3662c6a7dff6a171a55823d
Breno Machado <brenomachado.3064@gmail.com>
2025-06-03 17:01:14 -0300

    Adiciona atributos da Escolaridade e seus Getters/Setters

--- main/java/Escolaridade.java
+++ main/java/Escolaridade.java
@@ -1,2 +1,53 @@
 public class Escolaridade {
+
+    private boolean educacaoBasica;
+    private boolean ensinoMedio;
+    private boolean graduacao;
+    private boolean mestrado;
+    private boolean doutorado;
+
+
+
+
+
+
+    public boolean isEducacaoBasica() {
+        return this.educacaoBasica;
+    }
+
+    public void setEducacaoBasica(boolean educacaoBasica) {
+        this.educacaoBasica = educacaoBasica;
+    }
+
+    public boolean isEnsinoMedio() {
+        return this.ensinoMedio;
+    }
+
+    public void setEnsinoMedio(boolean ensinoMedio) {
+        this.ensinoMedio = ensinoMedio;
+    }
+
+    public boolean isGraduacao() {
+        return this.graduacao;
+    }
+
+    public void setGraduacao(boolean graduacao) {
+        this.graduacao = graduacao;
+    }
+
+    public boolean isMestrado() {
+        return this.mestrado;
+    }
+
+    public void setMestrado(boolean mestrado) {
+        this.mestrado = mestrado;
+    }
+
+    public boolean isDoutorado() {
+        return this.doutorado;
+    }
+
+    public void setDoutorado(boolean doutorado) {
+        this.doutorado = doutorado;
+    }
 }
