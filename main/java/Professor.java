b05ca7da155ea579f57947ea1c22016d953de280
Breno Machado <brenomachado.3064@gmail.com>
2025-06-03 17:46:24 -0300

    Adiciona no professor um método para verificar sua escolaridade analisando qual sua graduação

--- main/java/Professor.java
+++ main/java/Professor.java
@@ -3,5 +3,14 @@ public class Professor extends Pessoa{
     Professor(String nome, Escolaridade escolaridade)
     {
         super(nome);
+        setEscolaridade(escolaridade);
+    }
+
+    public String getNomeGraduacao()
+    {
+        if (getEscolaridade() == null) {
+            throw new IllegalStateException("O professor deve ter uma escolaridade!");
+        }
+        return getEscolaridade().getPosGraduacao();
     }
 }
